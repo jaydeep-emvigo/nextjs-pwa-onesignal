6ac973917411c141665f1eb174d7d6ed0d383ecb
jaydeep.karena.emvigo <jaydeep.karena@emvigotech.com>
2023-05-16 13:59:32 +0530

    integrated next-pwa

--- next.config.js
+++ next.config.js
@@ -3,4 +3,11 @@ const nextConfig = {
   reactStrictMode: false,
 }
 
-module.exports = nextConfig
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+})
+
+module.exports = withPWA(nextConfig)
